c9eb165af3764de68f8d914db4face323078eaa6
Frank Kobe <fkobe3@gmail.com>
2022-10-15 23:46:06 -0400

    attempting resolution by new start points

--- server.js
+++ server.js
@@ -8,16 +8,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.use(require('./routes'));
-
 // tell Mongoose which database to connect to
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social_api', {
-    useFindAndModify: false,
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/hey-you-social-media', 
+{
     useNewUrlParser: true,
     useUnifiedTopology: true
-});
+}
+);
 
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
+
+app.use(require('./routes'));
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
